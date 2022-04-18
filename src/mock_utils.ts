76f86272a1c8b8ad0fa14b834fb7418b0558dda2
Jonas Wrede <jonas.wrede@beyond365.de>
2022-04-18 04:32:10 +0200

    Added more endpoints

--- src/mock_utils.ts
+++ src/mock_utils.ts
@@ -1,11 +1,23 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
+import { JSONRPCError, JSONRPCResponse } from './types/jsonRPC';
+
 // This sets the mock adapter on the default instance
 const mock = new MockAdapter(axios);
-export const mockHM = (url: string, method: string, params: unknown) =>
-  mock.onPost(url, {
-    version: '1.1',
-    method: method,
-    params: params,
-  });
+
+export function mockHM<Result>(
+  url: string,
+  method: string,
+  params: unknown,
+  result: null | Result = null,
+  error: JSONRPCError = null
+) {
+  return mock
+    .onPost(url, {
+      version: '1.1',
+      method,
+      params,
+    })
+    .reply<JSONRPCResponse<Result>>(200, { version: '1.1', result, error });
+}
